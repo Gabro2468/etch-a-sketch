519518364d9d076d49fb729e6fd10dd6bf798531
Gabro2468 <mylitle43@gmail.com>
2023-02-09 13:21:54 +0100

    Change column declaration

--- scrypt.js
+++ scrypt.js
@@ -1,57 +1,11 @@
 const container = document.querySelector('#container');
 
-const column1 = document.createElement('div');
-const column2 = document.createElement('div');
-const column3 = document.createElement('div');
-const column4 = document.createElement('div');
-const column5 = document.createElement('div');
-const column6 = document.createElement('div');
-const column7 = document.createElement('div');
-const column8 = document.createElement('div');
-const column9 = document.createElement('div');
-const column10 = document.createElement('div');
-const column11 = document.createElement('div');
-const column12 = document.createElement('div');
-const column13 = document.createElement('div');
-const column14 = document.createElement('div');
-const column15 = document.createElement('div');
-const column16 = document.createElement('div');
+let numberGrid = 16
+let columnArray = [numberGrid]
+let rowArray = [numberGrid]
 
-column1.classList.add('column')
-column2.classList.add('column')
-column3.classList.add('column')
-column4.classList.add('column')
-column5.classList.add('column')
-column6.classList.add('column')
-column7.classList.add('column')
-column8.classList.add('column')
-column9.classList.add('column')
-column10.classList.add('column')
-column11.classList.add('column')
-column12.classList.add('column')
-column13.classList.add('column')
-column14.classList.add('column')
-column15.classList.add('column')
-column16.classList.add('column')
-
-container.appendChild(column1)
-container.appendChild(column2)
-container.appendChild(column3)
-container.appendChild(column4)
-container.appendChild(column5)
-container.appendChild(column6)
-container.appendChild(column7)
-container.appendChild(column8)
-container.appendChild(column9)
-container.appendChild(column10)
-container.appendChild(column11)
-container.appendChild(column12)
-container.appendChild(column13)
-container.appendChild(column14)
-container.appendChild(column15)
-container.appendChild(column16)
-
-
-
-
-
+for(let i=0; i<numberGrid; i++){
+    columnArray[i] = document.createElement('div');
+    columnArray[i].classList.add('column')
+    container.appendChild(columnArray[i]);
+}
