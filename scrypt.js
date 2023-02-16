65458be2a08fbcb0e3ca750ff24940f73122b863
Gabro2468 <mylitle43@gmail.com>
2023-02-16 11:21:48 +0100

    Finish a grid 16x16 with changeable amount

--- scrypt.js
+++ scrypt.js
@@ -6,6 +6,12 @@ let rowArray = [numberGrid]
 
 for(let i=0; i<numberGrid; i++){
     columnArray[i] = document.createElement('div');
-    columnArray[i].classList.add('column')
+    columnArray[i].classList.add('column');
     container.appendChild(columnArray[i]);
+    let column = columnArray[i];
+    for(let j=0; j<numberGrid; j++){
+        rowArray[j] = document.createElement('div');
+        rowArray[j].classList.add('square');
+        column.appendChild(rowArray[j]);
+    }
 }
